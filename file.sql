06c1b5eaa52243c1fbb52995cd027785701abf06
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-12-21 14:05:50 +0100

    finito

--- file.sql
+++ file.sql
@@ -25,6 +25,29 @@ where national_day is null
 
 Per ogni nazione, in ordine alfabetico, selezionare il nome, la regione e il continente
 select c.name,r.name,c2.name from countries c
-join regions r on c.country_id=r.region_id
-join continents c2 on c.country_id=c2.continent_id
-order by c.name asc, r.name asc,c2.name asc 
+join regions r on c.region_id =r.region_id
+join continents c2 on r.continent_id=c2.continent_id 
+order by c.name asc
+
+Selezionare le lingue ufficiali dell’Albania
+select language_id from country_languages
+where country_id=5 and official=1 
+
+Selezionare il Gross domestic product (GDP) medio dello United Kingdom tra il 2000 e il 2010
+select c.name,cs.gdp from country_stats cs 
+join countries c on cs.country_id=c.country_id
+where (c.name="United States") and (cs.`year` >2000 and cs.`year` <2010)
+
+
+Selezionare tutte le nazioni in cui si parla hindi, ordinate dalla più estesa alla meno estesa
+select * from countries c 
+join country_languages cl on c.country_id=cl.country_id
+join languages l on cl.language_id=l.language_id
+where l.`language`="Hindi"
+order by c.area desc
+
+
+Modificare la nazione di nome Italy, inserendo come national day il 2 giugno 1946
+UPDATE countries 
+SET national_day ='1946-06-02'
+WHERE name='Italy';
